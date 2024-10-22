f41d77128c099f6a5c442e1b14a65e5409c668f5
marialuiz4a <161467446+marialuiz4a@users.noreply.github.com>
2024-10-22 09:13:30 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,32 @@
+criaCartao(
+    'cantor',
+   'justin é um divo?',
+    'yes #sorrynada'
+)
+criaCartao(
+    'doces',
+    'chocolate white ou black',
+    'black obvio'
+)
+criaCartao(
+    'comida',
+    'strogonoff é melhor de frango ou carne?',
+    'frango obvio'
+)
+criaCartao(
+    'amizades',
+    'a gaby e a isa são duas divas?',
+    'yes'
+)
+
+criaCartao(
+    'cantor',
+    'eminem é um divo divonico?',
+    'yes yes!'
+)
+
+criaCartao(
+    'desenho',
+    'a clawdeen wolf é a mais diva de monster high sim ou claro',
+    'COM CERTEZA!!!!!!'
+)
